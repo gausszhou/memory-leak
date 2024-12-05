de94a969b2b09b0ea086f2b864e3b8677de91af7
gausszhou <gausszhou@qq.com>
2024-12-05 21:04:23 +0800

    initial commit

---
+++ src/components/MainContent.vue
@@ -0,0 +1,18 @@
+<template>  
+  <main>  
+    <router-view></router-view>  
+  </main>  
+</template>  
+
+<script>  
+export default {  
+  name: 'MainContent',  
+}  
+</script>  
+
+<style scoped>  
+main {  
+  padding: 1em;
+  background-color: #fff;
+}  
+</style>
---
+++ src/components/MainMenu.vue
@@ -0,0 +1,33 @@
+<template>
+  <aisde>
+    <ul>
+      <li v-for="route in routes" :key="route.name">
+        <router-link :to="route.path">{{ route.name }}</router-link>
+      </li>
+    </ul>
+  </aisde>
+</template>
+
+<script lang="ts" setup>
+import { routes } from '@/router';
+</script>
+
+<script lang="ts">
+export default {
+  name: "MainMenu",
+};
+</script>
+
+<style scoped>
+ul {
+  padding: 12px 4px;
+}
+li {
+  text-align: left;
+  padding-left: 4px;
+  cursor: pointer;
+}
+li:hover {
+  color: #42b883;
+}
+</style>
