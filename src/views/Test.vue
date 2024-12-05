b547a682f0f591db3ce516bad5fc09d51cf98391
gausszhou <gausszhou@qq.com>
2024-12-05 21:55:37 +0800

    test: composables

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -10,31 +10,6 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from "vue";
-
-function createArray(sizeInMB = 10) {
-  const sizeInBytes = sizeInMB * 1024 * 1024; // 10 * 1024 * 1024
-
-  // 创建类型数组
-  const data = new Uint8Array(sizeInBytes);
-
-  // 填充数组（可选）
-  for (let i = 0; i < data.length; i++) {
-    data[i] = i % 256; // 示例：用 0-255 的数字填充
-  }
-  return data;
-}
-
-function createList(count: number, callback: Function) {
-  return Array.from({ length: count }).map(() => callback());
-}
-
-const longList = ref<string[]>([]);
-let bigArray: Uint8Array = new Uint8Array(0);
-
-onMounted(() => {
-  longList.value = createList(1000, () => "0123456789");
-  bigArray = createArray(10);  
-  console.log(bigArray.length);
-});
+import { useLongList } from '@/composables/useLongList'
+const { longList } = useLongList(1000);
 </script>
